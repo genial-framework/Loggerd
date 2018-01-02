db7573da4e739b1412271b45469ddaa8abf52e8d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 00:25:17 -0500

    Update LoggerTest.php

--- tests/LoggerTest.php
+++ tests/LoggerTest.php
@@ -1,1 +1,61 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Logger> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Logger/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Logger;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * LoggerTest.
+ */
+final class LoggerTest extends TestCase
+{
+    /**
+     * test__invoke().
+     *
+     * Test the logger switch dispatcher
+     *
+     * @return void
+     */
+    public function test__invoke()
+    {
+        $logger = new Logger;
+        $this->assertTrue($logger);
+    }
+  
+    /**
+     * test__invoke2().
+     *
+     * Test the logger switch dispatcher
+     *
+     * @return void
+     */
+    public function test__invoke2()
+    {
+        $logger = new Logger;
+        $this->assertTrue($logger(false));
+    }
+  
+    /**
+     * test__invoke3().
+     *
+     * Test the logger switch dispatcher
+     *
+     * @return void
+     */
+    public function test__invoke3()
+    {
+        $logger = new Logger;
+        $this->assertTrue($logger(true));
+    }
+  
+}
