b54865bf1c9a3200f6a713b4b228e2af687d950a
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-02 01:23:00 -0500

    Update Transmitter.php

--- src/Transmitter.php
+++ src/Transmitter.php
@@ -1,1 +1,39 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Logger> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Logger/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Logger;
+
+/**
+ * Transmitter.
+ */
+class Transmitter extends Connector
+{
+  
+    /**
+     * write().
+     *
+     * Write a log to the log file.
+     *
+     * @param string|null $name    The name of the log.
+     * @param int|0 $level         The level the log has.
+     * @param string|null $message The message of the log.
+     *
+     * @return bool|true True if the log successfully logged.
+     */
+    public function write(string $name = null, $level = 0, string $message = null)
+    {
+        /* ... */
+        self::connectToWrite($name, $level, $message)
+        return true;
+    }
+  
+}
