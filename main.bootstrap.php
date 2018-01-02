f9a988e3a45427f473c48940c575631a2c7841cb
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-01 23:13:49 -0500

    Update main.bootstrap.php

--- main.bootstrap.php
+++ main.bootstrap.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Logger> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Logger/blob/master/LICENSE> New BSD License.
+ */
 
+define('LOGGER_DEPENDENT_ACTIVE', true);
+
+use Genial\Logger\Logger;
+
+$logger = new Logger;
+$logger(true);
